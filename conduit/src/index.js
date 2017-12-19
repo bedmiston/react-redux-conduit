5ca9358728cba91fa9c4f270a89bd149d9d1ec9d
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2017-12-19 16:10:46 -0600

    Re-order imports

--- conduit/src/index.js
+++ conduit/src/index.js
@@ -1,7 +1,7 @@
-import App from './App';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
+import App from './App';
 import store from './store';
 
 ReactDOM.render((
