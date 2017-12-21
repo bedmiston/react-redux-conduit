c0ed70bb324b981dd35c77710cfa936b74cdde20
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2017-12-21 12:40:59 -0600

    Add ability to register

--- conduit/src/index.js
+++ conduit/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import store from './store';
 import Home from './components/Home';
 import Login from './components/Login';
+import Register from './components/Register';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 ReactDOM.render((
@@ -13,6 +14,7 @@ ReactDOM.render((
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="login" component={Login} />
+        <Route path="register" component={Register} />
       </Route>
     </Router>
   </Provider>
