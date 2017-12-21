d24da5d2d2fca4f4fcf2453786ed9210296cf3ec
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2017-12-21 14:55:55 -0600

    Add the settings form

--- conduit/src/reducers/common.js
+++ conduit/src/reducers/common.js
@@ -14,6 +14,13 @@ export default (state = defaultState, action) => {
       };
     case 'REDIRECT':
       return { ...state, redirectTo: null };
+    case 'LOGOUT':
+      return {
+        ...state,
+        redirectTo: '/',
+        token: null,
+        currentUser: null
+      };
     case 'LOGIN':
     case 'REGISTER':
       return {
